1d1b19dedce82413e4542f4e033273dfadc1c684
Luxuryentertainmentcrowdtech <psicometafisica@gmail.com>
2025-11-28 19:52:12 -0300

    Create Spinner.tsx

---
+++ src/components/common/Spinner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface SpinnerProps {
+  size?: 'sm' | 'md' | 'lg';
+}
+
+const Spinner: React.FC<SpinnerProps> = ({ size = 'md' }) => {
+  const sizeClasses = {
+    sm: 'h-5 w-5 border-2',
+    md: 'h-8 w-8 border-4',
+    lg: 'h-16 w-16 border-4',
+  };
+
+  return (
+    <div
+      className={`animate-spin rounded-full border-solid border-current border-r-transparent align-[-0.125em] text-tec-blue motion-reduce:animate-[spin_1.5s_linear_infinite] ${sizeClasses[size]}`}
+      role="status"
+    >
+      <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+        Cargando...
+      </span>
+    </div>
+  );
+};
+
+export default Spinner;
